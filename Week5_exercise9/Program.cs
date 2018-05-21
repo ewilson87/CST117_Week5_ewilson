6686ad51a8b1c96ce0dc75e29b744e392212aec9
ewilson87 <32503240+ewilson87@users.noreply.github.com>
2018-05-21 16:48:27 +0600

    Add files via upload

---
+++ Week5_exercise9/Program.cs
@@ -0,0 +1,43 @@
+﻿//Corrected by Evan Wilson
+
+using System;
+
+namespace CST117_IC08_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //duplicate set A to perform union, since list is passed by reference once action is performed, A is changed. Perform union on _A
+            Set _A = new Set();
+            _A = A.DuplicateSet(A, _A);
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            //Performing union on Set A permenently changes set, changed union to be performed on clone of A, _A
+            //Console.WriteLine("A union B: " + A.union(B));
+            Console.WriteLine("A union B: " + _A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+            //prevents console from closing to user to view. Used this to see what was being written to console for debug instead of breakpoints
+            Console.Read();
+        }
+    }
+}
---
+++ Week5_exercise9/Set.cs
@@ -0,0 +1,83 @@
+﻿//Corrected by Evan Wilson
+
+using System.Collections.Generic;
+//Lydia's code - find the errors!
+namespace CST117_IC08_console
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        //doesn't need return type, changed bool to void
+        //public bool addElement(int val )
+        public void addElement(int val)
+        {
+            //streamlined code to reflect void instead of bool
+            //if (containsElement(val)) return false;
+            //else
+            //{
+            //    elements.Add(val);
+            //    return true;
+            //}
+            if (!containsElement(val))
+            {
+                elements.Add(val);              
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                //prevented proper loop execution by always returning a value in first iteration
+                //else return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        //method is never used
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                this.addElement(rhs.elements[i]);
+            }
+            //changed return to this from rhs
+            //return rhs
+            return this;
+        }
+
+        //new method to duplicate a Set to perform union on duplicate instead of original
+        public Set DuplicateSet(Set A, Set _A)
+        {
+            for (int i = 0; i < A.elements.Count; i++)
+            {
+                _A.addElement(A.elements[i]);
+            }    
+            return _A;
+        }
+    }
+}
