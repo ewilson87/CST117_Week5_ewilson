9812ad70c868fbc7e6be8d4c8a9063a3e5bce775
ewilson87 <32503240+ewilson87@users.noreply.github.com>
2018-05-28 09:22:27 +0600

    Add files via upload

---
+++ Week5Exercise7.cs
@@ -0,0 +1,48 @@
+﻿/**
+ * Program: Method Stubs - Week 5 Exercise 7
+ * File: MethodStubs.cs
+ * Summary: Various method stubs
+ * Author: Evan Wilson
+ * Date: May 23th, 2018
+ **/
+ 
+ using System;
+
+public class MethodStubs
+{
+    public Class1()
+    {
+        //1
+        public void Sum2Ints(int int1, int int2) {}
+
+        //2
+        public double Average5Doubles(double double1, double double2, double double3, double double4, double double5){}
+
+        //3
+        public int Sum2RandomNumbers(){}
+
+        //4
+        public bool DivisibleBy3(int int1, int int2, int int3){}
+
+        //5
+        public void SmallerString(string string1, string string2){}
+
+        //6
+        public double LargestDouble(double[] doubleArray){}
+
+        //7
+        public int[] ReturnIntArray(){}
+
+        //8
+        public bool IsBoolSame(bool bool1, bool bool2){}
+
+        //9
+        public double CalculateProduct(int int1, double double1){}
+
+        //10
+        public int Average2DIntArray(int[,] int2DArray){}
+
+        //10 alternate to return double average from int array
+        public double Average2dIntArray(int[,] int2DArray){}
+    }
+}
